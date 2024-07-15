cbc58896618408e95f57a9ceafb8a70427e3dad4
eduvinagre <eduvinagre@gmail.com>
2024-07-15 13:33:10 -0300

    initial commit

---
+++ src/routes/phoneCatalogRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+//LEMBRAR DE IMPORTAR O CONTROLLER
+
+//ATENÇÃO!!!! ROTAS PRECISAM SER ATUALIZADAS!!!!
+
+router.get('/', );
+router.post('/', );
+router.patch('/',);
+router.delete('/', );
+
+module.exports = router;
+
---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/database');
+const phoneCatalogRoutes = require('./routes/phoneCatalogRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/phone_catalog', phoneCatalogRoutes);
+
+sequelize.sync({ force: true }).then(() => {
+  console.log('Database connected');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+
