c9d68683f3181d2ab0020a5861a32bf546fc4609
Esther Guimarães <estherguimaraesl@gmail.com>
2024-07-16 18:50:29 -0300

    feat: products-id-endpoint

--- src/routes/phoneCatalogRoutes.js
+++ src/routes/phoneCatalogRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts } = require('../controllers');
+const { getProducts, getProductById } = require('../controllers');
 
 //ATENÇÃO!!!! ROTAS PRECISAM SER ATUALIZADAS!!!!
 
 router.get('/products', getProducts);
+router.get('/products/:id', getProductById);
 router.post('/');
 router.patch('/');
 router.delete('/');
