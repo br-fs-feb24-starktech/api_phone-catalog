74da0e32c4cf9bc8c8e83eaab9a646e080fb1512
mfcastilho <fredericocastilho@hotmail.com>
2024-07-16 02:19:05 -0300

    feat: create the files that will populate the database

---
+++ src/config/old/usersTableInfos.js
@@ -0,0 +1,38 @@
+const currentDate = new Date();
+
+const users = [
+  {
+    id: 1,
+    username: 'Esther Guimarães',
+    email: 'esther@email.com',
+    password: '123456',
+    created_at: currentDate,
+    updated_at: currentDate,
+  },
+  {
+    id: 2,
+    username: 'Eduardo Vinagre',
+    email: 'vinagre@email.com',
+    password: '123456',
+    created_at: currentDate,
+    updated_at: currentDate,
+  },
+  {
+    id: 3,
+    username: 'Higor Arruda',
+    email: 'higor@email.com',
+    password: '123456',
+    created_at: currentDate,
+    updated_at: currentDate,
+  },
+  {
+    id: 4,
+    username: 'Mario Frederico',
+    email: 'mariofrederico@email.com',
+    password: '123456',
+    created_at: currentDate,
+    updated_at: currentDate,
+  },
+];
+
+module.exports = users;
