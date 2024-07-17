534172bff36cddc7482fb68a2d8655570034deb8
HigorPedroso <higor533@gmail.com>
2024-07-17 19:14:44 -0300

    improve route

--- src/routes/phoneCatalogRoutes.js
+++ src/routes/phoneCatalogRoutes.js
@@ -10,7 +10,7 @@ router.get('/new-models', getNewModels);
 router.get('/products', validateQueryParams, getProducts);
 router.get('/products/:id', getProductById);
 router.get('/discount-models', getDiscountModels);
-router.get('/favorites', getFavorites);
+router.get('/favorites/:user', getFavorites);
 router.post('/favorites', postNewFavorite);
 
 module.exports = router;
