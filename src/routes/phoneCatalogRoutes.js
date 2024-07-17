5e95941a7bb4025bebb4ab851965ba9891439701
mfcastilho <fredericocastilho@hotmail.com>
2024-07-17 11:12:50 -0300

    feat: create get products route

--- src/routes/phoneCatalogRoutes.js
+++ src/routes/phoneCatalogRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getNewModels } = require('../controllers/phonesController');
+const { getProducts } = require('../controllers/ProductsController');
+const { validateQueryParams } = require('../middlewares');
 
 router.get('/new-models', getNewModels);
+router.get('/products', validateQueryParams, getProducts);
 
 module.exports = router;
