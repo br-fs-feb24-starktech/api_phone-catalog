f08e49898bde257692ca01d6d8cf6148f9f668e1
HigorPedroso <higor533@gmail.com>
2024-07-17 18:04:32 -0300

    favorites api

--- src/routes/phoneCatalogRoutes.js
+++ src/routes/phoneCatalogRoutes.js
@@ -4,10 +4,13 @@ const { getNewModels } = require('../controllers/phonesController');
 const { getProducts, getProductById } = require('../controllers/productController');
 const { validateQueryParams } = require('../middlewares');
 const { getDiscountModels } = require('../controllers/discountController');
+const { getFavorites, postNewFavorites } = require('../controllers/favoritesController');
 
 router.get('/new-models', getNewModels);
 router.get('/products', validateQueryParams, getProducts);
 router.get('/products/:id', getProductById);
 router.get('/discount-models', getDiscountModels);
+router.get('/favorites', getFavorites);
+router.post('/favorites', postNewFavorites);
 
 module.exports = router;
