6dd577b4c4c2b1a048fbd29fae5297f9970aca07
mfcastilho <fredericocastilho@hotmail.com>
2024-07-19 14:35:57 -0300

    feat: create validateUserRegisterFields middleware

---
+++ src/middlewares/validadeUserRegisterFields.js
@@ -0,0 +1,11 @@
+const validadeUserRegisterFields = (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  return next();
+}
+
+module.exports = validadeUserRegisterFields;
